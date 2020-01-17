91fa3f6aa059ceb71e804d5077d3c1ad60561eb9
bondhansarker <bondhansarker100@gmail.com>
2020-01-17 21:32:52 +0600

    User access added

--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -54,7 +54,7 @@ class Manage::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :phone, :role, :password)
+    params.require(:user).permit(:name, :email, :phone, :role, :password,:password_confirmation)
   end
 
 
