91f99effe91e69e8ab54e33a032490e0748956ca
bondhansarker <bondhansarker100@gmail.com>
2020-03-06 22:09:04 +0600

    login created

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,77 +1,25 @@
-# frozen_string_literal: true
+class Api::RegistrationsController < ApplicationController
 
-class Users::RegistrationsController < Devise::RegistrationsController
-   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
+  skip_before_action :verify_authenticity_token
 
   def create
     user = User.find_by(email: params[:email])
     if user.present?
       render json: {message: "User exists"}, status: 422 and return
     end
-
-    if user.valid_password?(params[:password])
-      token = user.token
-      unless token.present?
-        loop do
-          token = Devise.friendly_token
-          unless User.exists?(token: token)
-            user.update({token: token})
-            break token
-          end
-        end
-      end
-
-      render json: {token: token}, status: 200
+    user = User.new(email: params[:email],
+                    password: params[:password],
+                    password_confirmation: params[:password_confirmation],
+                    name: params[:name],
+                    phone: params[:phone],
+                    role: params[:role],
+                    target_amount: params[:target_amount],
+                    bonus_percentage: params[:bonus_percentage])
+    if user.save
+      render json: {message: "Created Successfully"}, status: 200
     else
       render json: {message: "Email or password is invalid"}, status: 422
     end
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
